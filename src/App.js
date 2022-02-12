8662069c029976762b5e8bed04ec86e2db146f80
Will <dsilverfox98@gmail.com>
2022-02-11 23:03:14 -0300

    Cards na pagina principal ajustados

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import api from "./services/api";
+import './app.css'
 
 function App() {
 
-  const [pokemon, setPokemon] = useState('');
-
-  async function getPokemon(name) {
-    await api.get(`pokemon/${name}`).then(({ data }) => {
-      console.log('Resultado interno axios: ', data);
-      setPokemon(data);
-
-    });
-  }
+  const [pokemons, setPokemons] = useState([]);
 
   useEffect(() => {
-    getPokemon('togekiss');
+    async function getPokemons() {
+      const { data: { results } } = await api.get('/pokemon');
+      const resp = await Promise.all(results.map(({ url }) => api.get(url)));
+      const format = resp.map((req) => req.data);
+
+      setPokemons(format);
+    }
+
+    getPokemons();
   }, []);
 
-  const Ditto = () => {
-    getPokemon('ditto');
-  }
-
-  const Pikachu = () => {
-    getPokemon('pikachu');
-  }
-
   return (
-    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', width: '100vw', height: '100vh' }}>
-      <button onClick={Ditto} style={{ background: '#666' }}>Ditto</button>
-      <div>
-        {pokemon && <Card pokemon={pokemon} />}
+    <>
+      <h1 className="title">Pokedex</h1>
+      <div className="poke">
+        {pokemons.length > 0 && pokemons.map((pokemon) => {
+          return (
+            <Card pokemon={pokemon} />
+          )
+        })}
       </div>
-      <button onClick={Pikachu} style={{ background: '#666' }}>Pikachu</button>
-    </div>
+    </>
   );
 }
 
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -20,12 +20,12 @@ const Card = ({ pokemon }) => {
             <div className="flip-card-inner">
                 <div className="flip-card-front">
                     <h2 className="flip-card-title" >{pokemon.name.toUpperCase()}</h2>
-                    <img className="poke-img" src={pokemon.sprites.other.dream_world.front_default} alt='Pokemon' />
+                    <img className="poke-img-front" src={pokemon.sprites.other.dream_world.front_default} alt='Pokemon' />
                     <ul className="type-list">{pokeTypes}</ul>
                 </div>
                 <div className="flip-card-back">
                     <h2 className="flip-card-title" >{pokemon.name.toUpperCase()}</h2>
-                    <img className="poke-img" src={pokemon.sprites.front_default} alt='Pokemon' />
+                    <img className="poke-img-back" src={pokemon.sprites.front_default} alt='Pokemon' />
                     <ul className="status" >{pokeStatus}</ul>
                 </div>
             </div>
