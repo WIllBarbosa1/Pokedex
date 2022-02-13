5cd13186ada787059896411e4f9f8d83ecdb18a6
Will <dsilverfox98@gmail.com>
2022-02-12 23:21:44 -0300

    Tratado img error quando o src da imagem era null

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import PokeStatus from "../PokeStatus";
 import PokeType from "../PokeType";
 import './Card.css'
+import defaultImg from '../../assets/default.png'
 
 const Card = ({ pokemon }) => {
 
@@ -10,12 +11,19 @@ const Card = ({ pokemon }) => {
             <div className="flip-card-inner">
                 <div className="flip-card-front">
                     <h2 className="flip-card-title" >{pokemon.name.toUpperCase()}</h2>
-                    <img className="poke-img-front" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt='Pokemon' />
+                    <img className="poke-img-front" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = defaultImg;
+                    }} alt='Pokemon' />
                     <PokeType pokemon={pokemon} />
                 </div>
                 <div className="flip-card-back">
                     <h2 className="flip-card-title" >{pokemon.name.toUpperCase()}</h2>
-                    <img className="poke-img-back" src={pokemon.sprites.front_default} alt='Pokemon' />
+                    <img className="poke-img-back" src={`${pokemon.sprites.front_default}`} onError={({ currentTarget }) => {
+                        currentTarget.onerror = null;
+                        currentTarget.src = defaultImg;
+                        currentTarget.className = 'poke-img-back-error'
+                    }} alt='Pokemon' />
                     <PokeStatus pokemon={pokemon} />
                 </div>
             </div>
