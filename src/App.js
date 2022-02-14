7e98a3b40361556a6ee0110ab635982759c96fb4
Will <dsilverfox98@gmail.com>
2022-02-14 10:54:14 -0300

    Search por tipo e nome do pokemon

--- src/App.js
+++ src/App.js
@@ -4,31 +4,55 @@ import api from "./services/api";
 import './app.css';
 import './appResponsive.css';
 import PokeNave from "./components/PokeNav";
+import SearchBar from "./components/SearchBar";
 
 function App() {
 
   const [pokemons, setPokemons] = useState([]);
-  const [pages, setPages] = useState(0);
+  const [pages, setPages] = useState(1);
   const [selectedPage, setSelectedPage] = useState(0);
   const offSet = 24;
 
+
+  async function getPokemonsByType(pokemonType) {
+    const { data: { pokemon } } = await api.get(`/type/${pokemonType}`);
+    const resp = await Promise.all(pokemon.map(({ pokemon: { url } }) => api.get(url)));
+    const format = resp.map((req) => req.data);
+    setPokemons(format);
+    setPages(1);
+  }
+
   async function getMaxPokemonsNumber() {
-    const { data: { count } } = await api.get('https://pokeapi.co/api/v2/pokemon');
+    const { data: { count } } = await api.get('/pokemon');
     setPages(Math.round(count / offSet));
   }
 
-  getMaxPokemonsNumber()
+  async function getPokemons() {
+    const { data: { results } } = await api.get(`/pokemon?offset=${selectedPage * offSet}&limit=${offSet}`);
+    const resp = await Promise.all(results.map(({ url }) => api.get(url)));
+    const format = resp.map((req) => req.data);
+    setPokemons(format);
+  }
 
   useEffect(() => {
-    async function getPokemons() {
-      const { data: { results } } = await api.get(`/pokemon?offset=${selectedPage * offSet}&limit=${offSet}`);
-      const resp = await Promise.all(results.map(({ url }) => api.get(url)));
-      const format = resp.map((req) => req.data);
-      setPokemons(format);
-    }
-
+    getMaxPokemonsNumber();
     getPokemons();
-  }, [selectedPage])
+  }, [selectedPage]);
+
+  async function getPokemonByName(name, pokeType) {
+    console.log('Name: ', name);
+    console.log('Type: ', pokeType);
+    if (!name && !pokeType) {
+      getPokemons();
+      getMaxPokemonsNumber();
+    } else if (!pokeType) {
+      const { data } = await api.get(`/pokemon/${name}`);
+      setPokemons([data]);
+      setPages(1);
+    } else {
+      getPokemonsByType(pokeType);
+    }
+  };
 
   return (
     <>
@@ -37,6 +61,9 @@ function App() {
         <p >Bem vindo a minha pokedex, ela ainda está em desenvolvimento, mas estou gostado de como esta ficando.</p>
       </section>
       <div className="mainContent">
+        <div className="wrapperForm">
+          <SearchBar setResult={(name, pokeType) => getPokemonByName(name, pokeType)} />
+        </div>
         <main className="poke">
           {pokemons.length > 0
             ? pokemons.map((pokemon) => {
---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+import './SearchBar.css';
+import './SearchBarResponsive.css';
+
+const SearchBar = ({ setResult }) => {
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = ({ searchText, PokeType }) => {
+        setResult(searchText, PokeType);
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} className='searchBar'>
+                <select {...register('PokeType')}>
+                    <option value={''} selected>Type</option>
+                    <option value={'normal'}>Normal</option>
+                    <option value={'fighting'}>Fighting</option>
+                    <option value={'flying'}>Flying</option>
+                    <option value={'poison'}>Poison</option>
+                    <option value={'ground'}>Ground</option>
+                    <option value={'rock'}>Rock</option>
+                    <option value={'bug'}>Bug</option>
+                    <option value={'ghost'}>Ghost</option>
+                    <option value={'steel'}>Steel</option>
+                    <option value={'fire'}>Fire</option>
+                    <option value={'water'}>Water</option>
+                    <option value={'grass'}>Grass</option>
+                    <option value={'electric'}>Eletric</option>
+                    <option value={'psychic'}>Psychic</option>
+                    <option value={'ice'}>Ice</option>
+                    <option value={'dragon'}>Dragon</option>
+                    <option value={'dark'}>Dark</option>
+                    <option value={'fairy'}>Fairy</option>
+                    {/* <option value={'unknown'}>Type</option> */}
+                    <option value={'shadow'}>Shadow</option>
+                </select>
+                <div>
+                    <input {...register('searchText')} placeholder='Insert pokemon name or number' className="searchBarInput" />
+                    <input type={"submit"} value='Search' />
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default SearchBar;
